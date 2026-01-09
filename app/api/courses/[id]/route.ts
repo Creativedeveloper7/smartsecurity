2799bb77bde82022953891f192a9be383bc9ba99
ian.dev <nmwanik111@gmail.com>
2026-01-09 11:40:38 +0300

    fixed course fetching at booking

--- app/api/courses/[id]/route.ts
+++ app/api/courses/[id]/route.ts
@@ -6,10 +6,10 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = params;
+    const { id } = await params;
     
     console.log("Received request for course ID:", id);
     
